217b4e714040478df9240d3fc1314c089b55b8a9
mechanikal <251160@edu.p.lodz.pl>
2026-04-13 15:46:24 +0200

    Implement login page

---
+++ serwis-samochodowy/src/app/wave-background/wave-background.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'wave-background',
+  imports: [],
+  standalone: true,
+  templateUrl: './wave-background.html',
+  styleUrl: './wave-background.css',
+})
+export class WaveBackground {}
