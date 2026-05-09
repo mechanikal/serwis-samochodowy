ad55f1c3c0c41731967cdf69794abf07092592af
mechanikal <251160@edu.p.lodz.pl>
2026-05-09 19:38:41 +0200

    simplify site design

---
+++ frontend/src/app/components/repairs/repairs.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-repairs',
+  imports: [],
+  templateUrl: './repairs.html',
+  styleUrl: './repairs.css',
+})
+export class Repairs {}
