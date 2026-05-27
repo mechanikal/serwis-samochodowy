facc9410f9ca217fed1b6997c75f8bf88b8c3cf5
KacperOrkwiszewski <251197@edu.p.lodz.pl>
2026-05-27 19:11:54 +0200

    Add statistical raport shell

--- frontend/src/app/components/statistical-report/statistical-report.ts
+++ frontend/src/app/components/statistical-report/statistical-report.ts
@@ -1,9 +1,39 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+interface StatItem {
+  name: string;
+  count: string;
+}
+
+interface StatSection {
+  title: string;
+  items: StatItem[];
+}
 
 @Component({
   selector: 'app-statistical-report',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './statistical-report.html',
   styleUrl: './statistical-report.css',
 })
-export class StatisticalReport {}
+export class StatisticalReport {
+  sections: StatSection[] = [
+    {
+      title: 'statystyki występowania usterek',
+      items: [
+        { name: 'NAZWA USTERKI', count: 'ILOŚĆ' },
+        { name: 'NAZWA USTERKI', count: 'ILOŚĆ' },
+        { name: 'NAZWA USTERKI', count: 'ILOŚĆ' },
+      ],
+    },
+    {
+      title: 'statystyki wykonanych usług',
+      items: [
+        { name: 'NAZWA USŁUGI', count: 'ILOŚĆ' },
+        { name: 'NAZWA USŁUGI', count: 'ILOŚĆ' },
+        { name: 'NAZWA USŁUGI', count: 'ILOŚĆ' },
+      ],
+    },
+  ];
+}
